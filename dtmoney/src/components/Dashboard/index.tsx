a8539a8de21232eea3f57daa49c9673759f1118c
Adriel Garcia <adrielgarcia@live.com>
2021-03-25 18:56:06 -0300

    Initial commit

---
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { Summary } from "components/Summary"
+import { TransactionsTable } from "components/TransactionsTable"
+
+import { Container } from "./styles"
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  )
+}
