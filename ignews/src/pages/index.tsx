969025ae71e952586581e4bcf535d114ff35b2ab
Adriel Garcia <adrielgarcia@live.com>
2021-05-01 12:05:36 -0300

    Created subscribe button

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from 'components/SubscribeButton';
+
 import styles from './home.module.scss';
 
 const Home = () => {
@@ -21,6 +23,8 @@ const Home = () => {
             Get access about all the publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
