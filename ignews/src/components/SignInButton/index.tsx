609e773e2be8b4160b4bab0d461cf9daac656d58
Adriel Garcia <adrielgarcia@live.com>
2021-05-03 21:46:01 -0300

    Setup next-auth

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,23 +1,32 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  if (isUserLoggedIn) {
+  if (session) {
     return (
-      <button className={styles.signInButton} type="button">
+      <button
+        className={styles.signInButton}
+        type="button"
+        onClick={() => signOut()}
+      >
         <FaGithub color="#04d361" />
-        Adriel Garcia
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
     );
   }
 
   return (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
