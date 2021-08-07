9ff78fc15c0c0b47d9b3cf0e00d9870cc8941883
Adriel Garcia <adrielgarcia@live.com>
2021-08-07 18:57:57 -0300

    feat: authenticate user

--- authentication/client/src/context/AuthContext.tsx
+++ authentication/client/src/context/AuthContext.tsx
@@ -1,4 +1,14 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import Router from 'next/router';
+import { setCookie } from 'nookies'
+
+import { api } from "../services/api";
+
+interface User {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
 
 interface SignInCredentials {
   email: string;
@@ -8,6 +18,7 @@ interface SignInCredentials {
 interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
+  user: User;
 }
 
 const AuthContext = createContext({} as AuthContextData);
@@ -16,18 +27,39 @@ interface Props {
   children: ReactNode;
 }
 
+const thirtyDays = 60 * 60 * 24 * 30;
+
 export const AuthProvider = (props: Props) => {
   const { children } = props;
+  const [user, setUser] = useState<User>(null);
 
-  const isAuthenticated = false;
+  const isAuthenticated = Boolean(user);
 
   const signIn = async (data: SignInCredentials) => {
-    console.log(data);
+    try {
+      const { email, password } = data;
+      const response = await api.post('sessions', { email, password });
 
+      const { permissions, roles, token, refreshToken } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: thirtyDays,
+        path: '/',
+      });
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: thirtyDays,
+        path: '/',
+      });
+      
+      setUser({ email, permissions, roles });
+      Router.push('/dashboard')
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
