ff075d44dce7cf1e44c0e1c283b2f3bcc0aa24ba
Adriel Garcia <adrielgarcia@live.com>
2021-10-08 20:17:30 -0300

    feat: useMemo

--- performance/components/SearchResults.tsx
+++ performance/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import { ProductItem } from "./ProductItem";
 
 interface Props {
@@ -7,8 +9,20 @@ interface Props {
 export const SearchResults = (props: Props) => {
   const { results } = props
 
+  /**
+   * 1. Usado para operações pesadas (cálculos complexos)
+   * 2. Igualdade referencial (quando a gente repassa a informação para um componente filho)
+   */
+  const totalPrice =  useMemo(() => {
+    return results.reduce((acc, curr) => {
+      return acc + curr.price
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map(product => (
         <ProductItem key={product.id} product={product} />
       ))}
